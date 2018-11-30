533f661fcd91a1c4a1342ba27532ccb6c9e7bbe0
Joseph Molina <josephmolina3388@gmail.com>
2018-11-29 23:04:23 -0800

    Added return to MainActivity button and google services

--- app/src/main/java/e/josephmolina/risingmessenger/LoginActivity.kt
+++ app/src/main/java/e/josephmolina/risingmessenger/LoginActivity.kt
@@ -2,11 +2,16 @@ package e.josephmolina.risingmessenger
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        back_to_register_textView.setOnClickListener {
+            finish()
+        }
     }
 }
