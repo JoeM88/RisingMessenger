c6899207282aa8a78450dce7dd6add09676054a9
Joseph Molina <josephmolina3388@gmail.com>
2018-11-29 22:32:04 -0800

    Created Register and login screens

--- app/src/main/java/e/josephmolina/risingmessenger/MainActivity.kt
+++ app/src/main/java/e/josephmolina/risingmessenger/MainActivity.kt
@@ -1,12 +1,19 @@
 package e.josephmolina.risingmessenger
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        already_have_account_editTextview.setOnClickListener {
+            val loginIntent = Intent(this, LoginActivity::class.java)
+            startActivity(loginIntent)
+        }
     }
 }
