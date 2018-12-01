1333f71e5f4b0d7ba82382c56c681356983073e2
Joseph Molina <josephmolina3388@gmail.com>
2018-11-30 23:39:57 -0800

    Created LoginActivity

--- app/src/main/java/e/josephmolina/risingmessenger/LoginActivity.kt
+++ app/src/main/java/e/josephmolina/risingmessenger/LoginActivity.kt
@@ -2,6 +2,9 @@ package e.josephmolina.risingmessenger
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -10,6 +13,19 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        login_button.setOnClickListener {
+            val email = login_email_editText.text.toString()
+            val password = login_password_editText.text.toString()
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    Log.d("LOGIN", "signin with email successful")
+
+                } else {
+                    Toast.makeText(this, "Authentification failed", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
         back_to_register_textView.setOnClickListener {
             finish()
         }
